ebb554311be82c1df279d60066a310fbffab3408
Rahul Durgapal <rahuldurgapal191@gmail.com>
2024-02-02 23:36:19 +0530

    add comment

--- interior_project.sql
+++ interior_project.sql
@@ -27,6 +27,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
+CREATE database interior_project;
+
+use interior_project;
+
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
